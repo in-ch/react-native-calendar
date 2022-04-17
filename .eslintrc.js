6f4eedfff0ac21698b0e9373a44251db71d91ee8
in-ch <sxin2949@naver.com>
2022-04-17 20:25:39 +0900

    eslint&prettier 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,47 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+	'env': {
+		es6: true,
+		node: true,
+		jest: true,
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 12,
+		'sourceType': 'module',
+		project: './tsconfig.json',
+	},
+	plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+	'rules': {
+		indent: ['error', 2, { SwitchCase: 1 }],
+		quotes: ['error', 'single', { avoidEscape: true }],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'prettier/prettier': 'error',
+		'no-empty-function': 'off',
+		'@typescript-eslint/no-empty-function': 'off',
+		'react/display-name': 'off',
+		'react/prop-types': 'off',
+	},
+	settings: {
+		react: {
+		  version: 'detect',
+		},
+	  },
 };
